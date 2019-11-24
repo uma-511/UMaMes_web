85d61ce105bf2216c334cec50697a9bb8e71d6ab
DESKTOP-OGVO0PK\Lonel <511639411@qq.com>
2019-11-24 13:25:06 +0800

    #添加机台管理模块

---
+++ src/api/machine.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function add(data) {
+  return request({
+    url: 'api/machine',
+    method: 'post',
+    data
+  })
+}
+
+export function del(id) {
+  return request({
+    url: 'api/machine/' + id,
+    method: 'delete'
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/machine',
+    method: 'put',
+    data
+  })
+}
+
+export function downloadMachine(params) {
+  return request({
+    url: 'api/machine/download',
+    method: 'get',
+    params,
+    responseType: 'blob'
+  })
+}
