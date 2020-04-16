c874ba148d5fb7481efa7f3598e03f90bb8c1fc8
xjb <18038829596@189.cn>
2020-04-16 16:25:45 +0800

    # 修改配置文件

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 "use strict";
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.192.32:8000"',
+  BASE_API: '"http://192.168.192.250:8000"',
   HOST: '"http://192.168.192.32"'
 };
