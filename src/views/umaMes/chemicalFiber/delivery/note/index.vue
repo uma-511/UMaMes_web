06d8efea201beb858d25ce33912696e82d55c7a7
tianshangdexiaoniaoqun <dengzhaoxiong@126.com>
2020-07-30 16:06:10 +0800

    Merge branch 'dzx' of https://gitee.com/lonelymj/UMaMes_web
    # Conflicts:
    #	src/views/umaMes/chemicalFiber/delivery/note/index.vue

--- src/views/umaMes/chemicalFiber/delivery/note/index.vue
+++ src/views/umaMes/chemicalFiber/delivery/note/index.vue
@@ -412,7 +412,7 @@
               <el-input v-model="scope.row.totalNumber" :min="0" />
             </template>
           </el-table-column>
-          <el-table-column prop="realQuantity" label="数量" width="实收100px" align="center">
+          <el-table-column prop="realQuantity" label="实收数量" width="100px" align="center">
             <template slot-scope="scope">
               <el-input v-model="scope.row.realQuantity" :min="0" />
             </template>
@@ -940,6 +940,17 @@
           }
           const values = data.map(item => Number(item[column.property]))
           if (index === 6) {
+            sums[index] = values.reduce((prev, curr) => {
+              const value = Number(curr)
+              if (!isNaN(value)) {
+                return prev + curr
+              } else {
+                return prev
+              }
+            }, 0).toFixed(2)
+            sums[index] += ' 元'
+          }
+          if (index === 7) {
             sums[index] = values.reduce((prev, curr) => {
               const value = Number(curr)
               if (!isNaN(value)) {
