4eae640ec64b2c8674cecb573aca5d4b39ac6d4f
tianshangdexiaoniaoqun <dengzhaoxiong@126.com>
2020-08-04 13:22:35 +0800

    修改只显示一次保存

--- src/views/umaMes/chemicalFiber/delivery/note/index.vue
+++ src/views/umaMes/chemicalFiber/delivery/note/index.vue
@@ -1118,17 +1118,19 @@ export default {
         if (this.detailList[i].realQuantity == '' || this.detailList[i].realQuantity == null) {
           this.detailList[i].realQuantity = 0
         }
-        if (i = this.detailList.length && ifNull == true ) {
+        if (i < this.detailList.length && ifNull == true ) {
           editList(this.detailList).then(res => {
-            this.$notify({
-              title: '保存成功',
-              type: 'success',
-              duration: 2500
-            })
             this.init()
             this.detailLoading = false
           })
         }
+      }
+      if (ifNull) {
+        this.$notify({
+          title: '保存成功',
+          type: 'success',
+          duration: 2500
+        })
       }
     },
     detail(data) {
