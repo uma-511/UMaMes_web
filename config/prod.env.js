8b7ebead332cfb70ccd70218f48715e5363e2a28
tianshangdexiaoniaoqun <dengzhaoxiong@126.com>
2020-09-18 16:09:34 +0800

    修改了生产报表的时间

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"http://192.168.192.250:8000"',
   HOST: '"http://192.168.192.250"'
-  /*BASE_API: '"http://192.168.192.100:8000"',
-  HOST: '"http://192.168.192.100"'*/
+ /* BASE_API: '"http://192.168.88.101:8000"',
+  HOST: '"http://192.168.88.101"'*/
 };
